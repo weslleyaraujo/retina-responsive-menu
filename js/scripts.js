bbafe934a75d804b0cc0fa716dc3d5785111e36e
Weslley Araujo <weslleyaraujo@MacBook-Pro-de-usuario.local>
2013-07-13 15:09:08 -0300

    finalizado menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 window.onload = function(){
-
-	//  The function to change the class
+	     
+	//  The function to change the class
 	var changeClass = function (r,className1,className2) {
-		var regex = new RegExp("(?:^|\\s+)" + className1 + "(?:\\s+|$)");
-		if( regex.test(r.className) ) {
-			r.className = r.className.replace(regex,' '+className2+' ');
-	    }
-	    else{
-			r.className = r.className.replace(new RegExp("(?:^|\\s+)" + className2 + "(?:\\s+|$)"),' '+className1+' ');
-	    }
-	    return r.className;
-	};
-
+	    var regex = new RegExp("(?:^|\\s+)" + className1 + "(?:\\s+|$)");
+	    if( regex.test(r.className) ) {
+	        r.className = r.className.replace(regex,' '+className2+' ');
+	    }
+	    else{
+	        r.className = r.className.replace(new RegExp("(?:^|\\s+)" + className2 + "(?:\\s+|$)"),' '+className1+' ');
+	    }
+	    return r.className;
+	};  
+	 
+	//  Creating our button for smaller screens
 	var menuElements = document.getElementById('menu');
-	menuElements.insertAdjacentHTML('afterbegin', '<button type="button" id="menutoggle" class="navtoogle" aria-hidden="true"><i aria-hidden="true" class="icon-menu"> </i> Menu</button>');
-
+	menuElements.insertAdjacentHTML('afterBegin','<button type="button" id="menutoggle" class="navtoogle" aria-hidden="true"><i aria-hidden="true" class="icon-menu"> </i> Menu</button>');
+	 
+	//  Toggle the class on click to show / hide the menu
 	document.getElementById('menutoggle').onclick = function() {
-		console.log("ae");
-		console.log(changeClass(this, 'navtoggle active', 'navtoggle'));
-	} 
- 
-};
+	    changeClass(this, 'navtoogle active', 'navtoogle');
+	}
+}
