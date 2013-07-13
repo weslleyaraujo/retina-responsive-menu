31fc145928aa92745401eb995bd183db654bf254
Weslley Araujo <weslleyaraujo@MacBook-Pro-de-usuario.local>
2013-07-13 00:05:22 -0300

    initial commit

---
+++ css/icons/lte-ie7.js
@@ -0,0 +1,34 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-users' : '&#xe001;',
+			'icon-bookmark' : '&#xe002;',
+			'icon-home' : '&#xe003;',
+			'icon-pencil' : '&#xe004;',
+			'icon-busy' : '&#xe005;',
+			'icon-menu' : '&#xe000;',
+			'icon-mail' : '&#xe006;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
